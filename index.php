6e628c19c6193fc1b8e532bbb4b25bc5c94439aa
NadeerMukaram <emoprincenadeer@gmail.com>
2024-12-07 18:33:37 +0800

    s

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                 </div>
 
                 <div class="collegeNameContainer">
-                    <p>CSSPE Inventory & Information System</p>
+                    <p>CSSPE Yes Inventory & Information System</p>
                 </div>
             </div>
 
