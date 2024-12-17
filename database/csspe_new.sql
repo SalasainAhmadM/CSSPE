a3f37e56fa0de360e7f022854794799890bdb88e
NadeerMukaram <emoprincenadeer@gmail.com>
2024-12-18 03:24:24 +0800

    db

--- database/csspe_new.sql
+++ database/csspe_new.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2024 at 07:02 PM
+-- Generation Time: Dec 17, 2024 at 08:23 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,7 +40,14 @@ CREATE TABLE `announcements` (
 --
 
 INSERT INTO `announcements` (`id`, `title`, `description`, `date_uploaded_at`, `location`) VALUES
-(6, 'Nzro Day', 'nice', '2024-12-14 18:33:42', '     niceasdas');
+(6, 'Nzro Day', 'nice', '2024-12-14 18:33:42', '     niceasdas'),
+(8, 'Yes', 'hahahahahha', '2024-12-17 18:49:24', 'zamboanga city'),
+(9, 'yesssssssssssssss', 'hahhahahha', '2024-12-17 19:21:39', 'zamboanga city'),
+(10, 'yes', 'dd', '2024-12-17 20:01:12', 'zamboanga cityrr'),
+(11, 'asd', 'qwew', '2024-12-17 20:07:33', 'zamboanga cityeqwewe'),
+(12, 'gahahhhha', 'dasdas', '2024-12-17 20:15:49', 'hhaha'),
+(13, 'dasd', 'sada', '2024-12-17 20:18:51', 'zamboanga city'),
+(14, 'NICEEEEEEEEEEEEEEEe', 'eeeeeee', '2024-12-17 20:19:18', 'zamboanga city');
 
 -- --------------------------------------------------------
 
@@ -60,8 +67,7 @@ CREATE TABLE `departments` (
 --
 
 INSERT INTO `departments` (`id`, `department_name`, `description`, `image`) VALUES
-(18, 'qwe', 'qwe', '../assets/img/675c709414b53.jpg'),
-(20, 'yoboseyo', 'haaha', '../assets/img/CSSPE.png');
+(21, 'College of Architecture', 'yes', '../assets/img/CSSPE.png');
 
 -- --------------------------------------------------------
 
@@ -112,8 +118,47 @@ CREATE TABLE `memorandums` (
   `file_path` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL,
   `uploaded_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime DEFAULT NULL
+  `updated_at` datetime DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `memorandums`
+--
+
+INSERT INTO `memorandums` (`id`, `file_path`, `title`, `uploaded_at`, `updated_at`, `description`) VALUES
+(9, '../assets/uploads/6761741983914_Mukaram_CV.pdf', 'dasd', '2024-12-17 05:52:41', '0000-00-00 00:00:00', 'asd'),
+(15, '../assets/uploads/6761bb7f03e7d_CANDEN ATTACHMENTS-merged.pdf', 'd', '0000-00-00 00:00:00', NULL, 'd'),
+(16, '../assets/uploads/6761bbdab8b5e_1576632750_1568344676_Customer Request Form (Front) (1) (2).pdf', 'hahhaha', '2024-12-17 10:58:50', NULL, 'yesssssssssssssssssss'),
+(17, '../assets/uploads/6761c11f6267f_Mukaram_CV.pdf', 'yes', '2024-12-17 11:21:19', NULL, 'yesssssssssssss');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `uploaded_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `type` enum('Memorandums','Announcements') NOT NULL,
+  `is_read` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `notifications`
+--
+
+INSERT INTO `notifications` (`id`, `title`, `description`, `uploaded_at`, `type`, `is_read`) VALUES
+(3, 'yes', 'yesssssssssssss', '2024-12-17 11:21:19', 'Announcements', 1),
+(4, 'yesssssssssssssss', 'hahhahahha', '2024-12-17 11:21:39', 'Memorandums', 1),
+(5, 'yes', 'dd', '2024-12-17 12:01:12', 'Announcements', 1),
+(6, 'asd', 'qwew', '2024-12-17 12:07:33', 'Announcements', 1),
+(7, 'gahahhhha', 'dasdas', '2024-12-17 12:15:49', 'Announcements', 1),
+(8, 'dasd', 'sada', '2024-12-17 12:18:51', 'Announcements', 1),
+(9, 'NICEEEEEEEEEEEEEEEe', 'eeeeeee', '2024-12-17 12:19:18', 'Announcements', 1);
 
 -- --------------------------------------------------------
 
@@ -127,14 +172,6 @@ CREATE TABLE `organizations` (
   `description` text NOT NULL,
   `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `organizations`
---
-
-INSERT INTO `organizations` (`id`, `organization_name`, `description`, `image`) VALUES
-(10, 'qwed', 'qweas', '../assets/img/675c5398d477f.jpg'),
-(11, 'qwedffff', '123', '../assets/img/675c6ef4eed31.png');
 
 -- --------------------------------------------------------
 
@@ -185,9 +222,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `middle_name`, `email`, `address`, `contact_no`, `rank`, `password`, `created_at`, `role`, `image`, `department`) VALUES
-(47, 'Casca', 'Nadeer', 'R', 'nzro@gmail.com', 'Hannah Drive, Rio Hondo, Zamboanga City', '54141412', 'Instructor', '$2y$10$bgIY43.lS5YGOvMl2NajUea8PW8ntj357oYFGcpzbAGCubL9x0Wcm', '2024-12-12 15:41:00', 'instructor', '675c6f1032a0f.jpg', ''),
-(48, 'Nadeerrrrrr', 'Re', 'Mukaramrrrrrrrrrrrrrrr', 'zoro0wqewqewqe000@gmail.com', 'Hannah Drive, Rio Hondo, Zamboanga City', '12312312', 'Associate Professor', '$2y$10$jWoAT.aYeDvyj4i6VR6VNewlHLd.yEsrN9bCbur.bh5a1BE.vhgzS', '2024-12-13 15:52:27', 'Instructor', '675c66c8928f6.jpg', 'College of Architecture'),
-(54, 'Nzrooooo', 'Mkrmmmm', 'C', 'nadeer12345@gmail.com', 'Rio Hondo', '123123', 'Instructor', '$2y$10$loaz6TFbBPejBv3HgJAU6e0C4MPea9BvfbI.zdwboTCe24PIqmxYW', '2024-12-15 15:38:55', 'instructor', 'CSSPE.png', '');
+(47, 'Casca', 'Nadeer', 'R', 'nzro@gmail.com', 'Hannah Drive, Rio Hondo, Zamboanga City', '54141412', 'Instructor', '$2y$10$5LWJBHQEntvz6IvYzItoPuWySZiRMYOBjbFX1HoBx3vdtoismhJCW', '2024-12-12 15:41:00', 'instructor', '675c6f1032a0f.jpg', 'College of Architecture'),
+(56, 'Nzroooo', 'Mkrmoooo', 'R', 'nadeer12345@gmail.com', 'Rio Hondo', '213123', 'Instructor', '$2y$10$5LWJBHQEntvz6IvYzItoPuWySZiRMYOBjbFX1HoBx3vdtoismhJCW', '2024-12-17 18:03:21', 'information_admin', 'CSSPE.png', 'College of Architecture');
 
 --
 -- Indexes for dumped tables
@@ -225,6 +261,12 @@ ALTER TABLE `memorandums`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `organizations`
 --
 ALTER TABLE `organizations`
@@ -252,13 +294,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `announcements`
 --
 ALTER TABLE `announcements`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `departments`
 --
 ALTER TABLE `departments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `events`
@@ -276,25 +318,31 @@ ALTER TABLE `items`
 -- AUTO_INCREMENT for table `memorandums`
 --
 ALTER TABLE `memorandums`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `organizations`
 --
 ALTER TABLE `organizations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `pending_users`
 --
 ALTER TABLE `pending_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- Constraints for dumped tables
